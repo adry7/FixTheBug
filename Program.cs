d9c35e63d5d2f88ce4cbd8aa5342d86d5a2d7d38
Mustafa Adry <mustafa.adry2025@gmail.com>
2020-10-19 18:58:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixTheBug
+{
+    class Program
+    {
+        public static double Average(int a ,int b)
+        {
+
+            #region WrongImplementation
+            //  return a + b / 2;
+
+            //1- division has priority over addition
+            //2- int type will trim the fraction
+            #endregion
+
+
+
+
+
+            //fix 
+            return (a + b) / 2d;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(2, 1));
+        }
+    }
+}
